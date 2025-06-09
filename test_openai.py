9fe43a4a67da70201f75f7dcd76bc38e8aa15de3
Shakeeb Alireza <shakeeb.alireza@rezayat.net>
2025-06-09 02:48:58 +0100

    snap

--- test_openai.py
+++ test_openai.py
@@ -11,7 +11,7 @@ client = OpenAI(
 )
 
 response = client.responses.create(
-    model="o4-mini",
+    model="gpt-4.1-nano",
     instructions="You are a coding assistant that talks like a pirate.",
     input="How do I check if a Python object is an instance of a class?",
 )
